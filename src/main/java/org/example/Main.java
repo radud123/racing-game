76a666ee4e7e307727764e3fbd0522c56d08c778
Dumitrascu Radu <roctavian377@gmail.com>
2024-03-09 16:13:18 +0200

    clasa car

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world");
+        Car carReference = new Car();
+        carReference.name="Ferrari";
+        carReference.maxSpeed=300;
+        carReference.fuelLevel=60;
+        carReference.mileage=12.50;
+        carReference.totalTravelDistance=0;
+        carReference.damaged=false;
+        carReference.doorCount=1;
+        carReference.color="Red";
+        System.out.println("Properties of car "+ carReference.name);
+        //System.out.println(q);
+    Car car2= new Car();
+    car2.name="mazda";
+    car2.maxSpeed=200;
+    car2.fuelLevel=50;
+    car2.damaged=true;
+        System.out.println("Properties of car "+ car2.name);
+        System.out.println("Max speed "+car2.maxSpeed);
+        System.out.println("Fuel Level "+car2.fuelLevel);
+        System.out.println("Damage "+car2.damaged);
+        //System.out.println("");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
