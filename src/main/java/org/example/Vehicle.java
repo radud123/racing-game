9a377a6eadd8d4501fddedd05141bda978cb85b1
Dumitrascu Radu <roctavian377@gmail.com>
2024-03-11 16:04:28 +0200

    declaring methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -9,4 +9,16 @@ public class Vehicle {
     boolean damaged;
     String color;
 
+    public  double accelerate(double speed,double durationInHours){
+
+        System.out.println(name+ "is accelerating with "+speed +" km/h for "+ durationInHours+" h");
+        double distance=speed*durationInHours;
+        totalTravelDistance=totalTravelDistance+distance;
+        System.out.println("Total travel distance is "+name +":"+totalTravelDistance);
+        double usedFuel=distance*mileage/100;
+        fuelLevel=fuelLevel-usedFuel;
+        System.out.println("Remainig travel distance is "+name+":"+fuelLevel);
+
+        return distance;
+    }
 }
